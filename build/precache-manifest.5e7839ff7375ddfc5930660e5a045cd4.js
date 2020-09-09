93bc4eda2c662b43eca86ef4325578fdd60a6e04
trufflesprouts <abebaaali@gmail.com>
2020-09-08 20:31:47 -0500

    build

---
+++ build/precache-manifest.5e7839ff7375ddfc5930660e5a045cd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9674cd17ab4f7be59548723bbd22c8ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc585f5cc5a8b40ddbd",
+    "url": "/static/css/main.f0df3fd1.chunk.css"
+  },
+  {
+    "revision": "308c523b806d131188d0",
+    "url": "/static/js/2.a2a647df.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.a2a647df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fbc585f5cc5a8b40ddbd",
+    "url": "/static/js/main.eda8fac5.chunk.js"
+  },
+  {
+    "revision": "abbaefb54f6b808082f3",
+    "url": "/static/js/runtime-main.c398fd02.js"
+  }
+]);
