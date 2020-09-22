f1392790c318b37c25adc1100e7a8dcd1b083791
trufflesprouts <abebaaali@gmail.com>
2020-09-21 20:13:47 -0500

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5e7839ff7375ddfc5930660e5a045cd4.js"
+  "/precache-manifest.dfc3db68dded2c4ab2a40b89a68326b6.js"
 );
 
 self.addEventListener('message', (event) => {
