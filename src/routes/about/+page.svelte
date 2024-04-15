a4e182ceb44733f8e4990d754e0ffa988e17ce38
abe <abebaaali@gmail.com>
2024-04-14 23:21:20 -0500

    About me page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,26 @@
+<p>
+	<b>hi, i am abe</b>, a recent grad from the university of illinois urbana-champaign where i
+	studied computer science. currently i work as a full-stack software engineer at tesla. before that
+	i interned with the web core team at meta and shipped a few instagram direct and facebook web
+	features, and i interned with the sell-side customer growth team at redfin and built an app that
+	gets used by 70k users a day. i also worked on
+	<a target="_blank" rel="noopener noreferrer" href="https://www.karmatrade.com//">karma trade</a>
+	to develop an online cloth swapping platform, and
+	<a target="_blank" rel="noopener noreferrer" href="https://caramel.la/">caramella</a>
+	to create a new online publishing platform that’s focused on creating beautiful custom designs, i did
+	lots of things including full stack development, testing, and ui/ux design. you can find me on the
+	<a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/abe.baali/"
+		>instagrams</a
+	>, email me at abebaaali@gmail.com, check out
+	<a target="_blank" rel="noopener noreferrer" href="/static/cv.pdf">my résumé</a>
+	, or find me trufflesprouts on github. thank you for reading, pls like & subscribe!
+</p>
+<img src="/me.jpeg" alt="its me" />
+<p class="caption">pictured on the left</p>
+
+<style lang="scss">
+	img {
+		margin-top: 1rem;
+		max-width: 640px;
+	}
+</style>
