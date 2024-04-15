dcce8c7e50bcb5d91074d3081b40ffa65935f983
abe <abebaaali@gmail.com>
2024-04-15 00:02:29 -0500

    Add code

---
+++ src/routes/code/+page.svelte
@@ -0,0 +1,107 @@
+<div>
+	<ul>
+		<li>
+			<h3>tesla</h3>
+			<img src="/tesla.png" alt="" />
+		</li>
+		<li>
+			<h3>meta</h3>
+			<p>
+				shipped a few features for: <br />
+				- facebook.com <br />
+				- instagram.com/direct <br />
+			</p>
+		</li>
+		<li>
+			<h3>
+				redfin, how much house can i afford
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://www.redfin.com/how-much-house-can-i-afford"
+				>
+					(website)
+				</a>
+			</h3>
+			<video autoPlay loop muted playsInline>
+				<source src="/redfin.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+			<p>
+				increased seller leads by developing an interactive home affordability calculator to inform
+				buyers on how much they can afford.
+			</p>
+			<p>utilized: Javascript, React, Docker, Java, A/B testing.</p>
+		</li>
+		<li>
+			<h3>
+				karma trade
+				<a target="_blank" rel="noopener noreferrer" href="http://karmatrade.shop/"> (website) </a>
+			</h3>
+			<video autoPlay loop muted playsInline>
+				<source src="/karma.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+			<p>
+				developed an online cloth swapping platform, complete with a virtual currency, and a fully-
+				fledged e-commerce platform.
+			</p>
+			<p>
+				utilized: Javascript, React, Node, Git, Firebase, Web Payments (Square), Figma, Adobe
+				Illustrator.
+			</p>
+		</li>
+		<li>
+			<h3>
+				caramella
+				<a target="_blank" rel="noopener noreferrer" href="https://caramel.la/"> (website) </a>
+			</h3>
+			<video autoPlay loop muted playsInline>
+				<source src="/caramella.mp4" type="video/mp4" />
+				Your browser does not support the video tag.
+			</video>
+			<p>
+				worked in a product team of three to design and develop a website maker from scratch, using
+				a full javascript stack. my responsibilities included working in the front-end and back-end
+				of new features and supporting ongoing infrastructure maintenance.
+			</p>
+			<p>utilized: Javascript, AWS, MongoDB, Jest, GraphQL, TypeScript, SVG animations, DNS.</p>
+		</li>
+		<li>
+			<h3>
+				tamerny
+				<a
+					target="_blank"
+					rel="noopener noreferrer"
+					href="https://drive.google.com/drive/folders/1F9KkALPDbdjRDnrEnj2IGeCTQauZu6FX?usp=sharing"
+				>
+					(gallery)
+				</a>
+			</h3>
+			<img src="/tamerny.jpg" alt="" />
+			<p>designed and built an angular app that manages dipatchers of a delivery service.</p>
+		</li>
+	</ul>
+</div>
+
+<style lang="scss">
+	video,
+	img {
+		width: 100%;
+		max-width: 640px;
+		margin: 0.2rem 0;
+		padding: 0;
+	}
+	p {
+		max-width: 640px;
+	}
+	h3 {
+		font-size: 1rem;
+	}
+	li:not(:first-child) h3 {
+		margin-top: 1rem;
+	}
+	h2 {
+		margin-top: 1rem;
+	}
+</style>
